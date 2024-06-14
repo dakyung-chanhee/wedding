94bd2489c1e38f8890498c76b71fe262544af3c6
Chanhee <chanhee13p@gmail.com>
2024-06-14 16:38:59 +0900

    update layout using lorem ipsum text and plane images

---
+++ src/introduce-brideand-groom.js
@@ -0,0 +1,57 @@
+window.addEventListener("load", function () {
+  transformIntroductionHorizontally();
+  presentIntroductionDescription();
+});
+
+document.addEventListener("scroll", function () {
+  transformIntroductionHorizontally();
+  presentIntroductionDescription();
+});
+
+function transformIntroductionHorizontally() {
+  const section = document.querySelector("section.who");
+  const imageContainer = document.querySelector("section.who .who-image-container");
+  const firstImage = document.querySelector("section.who img:first-child");
+  const lastImage = document.querySelector("section.who img:last-child");
+
+  // 현재 스크롤 위치
+  const scrollY = document.documentElement.scrollTop || window.scrollY;
+
+  // 스크롤 시작 지점: 첫 번째 이미지가 중앙에 위치
+  const scrollStartX = firstImage.offsetLeft + firstImage.offsetWidth / 2 - window.innerWidth / 2;
+
+  // 스크롤 끝 지점: 마지막 이미지가 중앙에 위치
+  const scrollEndX = lastImage.offsetLeft + lastImage.offsetWidth / 2 - window.innerWidth / 2;
+
+  // 현재 위치가 섹션 위에 있는 경우
+  if (scrollY < section.offsetTop) {
+    imageContainer.style.transform = `translateX(${-scrollStartX}px)`;
+    return;
+  }
+
+  // 현재 위치가 섹션 아래에 있는 경우
+  if (scrollY > section.offsetTop + section.offsetHeight - window.innerHeight) {
+    imageContainer.style.transform = `translateX(${-scrollEndX}px)`;
+    return;
+  }
+
+  // 스크롤 비율에 따라 갤러리 이동
+  const scrollRatio = (scrollY - section.offsetTop) / (section.offsetHeight - window.innerHeight);
+  const nextX = scrollStartX + scrollRatio * (scrollEndX - scrollStartX);
+  imageContainer.style.transform = `translateX(${-nextX}px)`;
+}
+
+function presentIntroductionDescription() {
+  const descriptions = document.querySelectorAll("section.who .who-description");
+  const firstGalleryImage = document.querySelector("section.who img:first-child");
+  const { bottom: imageBottom } = firstGalleryImage.getBoundingClientRect();
+
+  descriptions.forEach((description) => {
+    const { top: descriptionTop } = description.getBoundingClientRect();
+    const aboveThanScreen = descriptionTop < window.innerHeight;
+    const belowThanImage = descriptionTop > imageBottom;
+    const show = belowThanImage && aboveThanScreen;
+
+    description.style.opacity = show ? 1 : 0;
+  });
+}
