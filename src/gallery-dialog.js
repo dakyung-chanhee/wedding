4d084779fe741799b05368d1945ce07e70173e86
Chanhee <chanhee13p@gmail.com>
2024-07-23 11:21:50 +0900

    #1 add prev and next button on the gallery dialog

--- src/gallery-dialog.js
+++ src/gallery-dialog.js
@@ -12,7 +12,26 @@ window.addEventListener('load', function () {
 
   galleryDialog.addEventListener('click', function (event) {
     if (event.target === galleryDialog) {
+      galleryDialogImage.src = '';
       galleryDialog.close();
     }
   });
 });
+
+function prevImage() {
+  const galleryDialogImage = document.querySelector('#gallery-dialog-image');
+  const galleryImages = document.querySelectorAll('.gallery-image');
+  const currentImageIndex = Array.from(galleryImages).findIndex((image) => image.src === galleryDialogImage.src);
+  if (currentImageIndex > 0) {
+    galleryDialogImage.src = galleryImages[currentImageIndex - 1].src;
+  }
+}
+
+function nextImage() {
+  const galleryDialogImage = document.querySelector('#gallery-dialog-image');
+  const galleryImages = document.querySelectorAll('.gallery-image');
+  const currentImageIndex = Array.from(galleryImages).findIndex((image) => image.src === galleryDialogImage.src);
+  if (currentImageIndex < galleryImages.length - 1) {
+    galleryDialogImage.src = galleryImages[currentImageIndex + 1].src;
+  }
+}
