94d4f4dbb8eb0be1f9a7c4b939805b713b943a5f
Chanhee <chanhee13p@gmail.com>
2024-11-19 17:24:42 +0900

    remove masonry layout

--- src/layout-gallery.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('load', function () {
-  const gallery = document.querySelector('section.gallery');
-  imagesLoaded(gallery, function () {
-    const masonry = new Masonry(gallery, {
-      itemSelector: 'img',
-    });
-  });
-});
